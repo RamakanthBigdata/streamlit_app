f0222fc5b8bddc1f3d1168d516b3e44656c9c593
RamakanthBigdata <ramakanth.padala@gmail.com>
2023-07-18 16:38:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blue Berry Oat Meal')
-streamlit.text('Kale Spinach and Rocket Smoothy')
-streamlit.text('Hard Boiled Free Range Egg')
+streamlit.header(' Breakfast Menu')
+streamlit.text(' 🥣 Omega 3 and Blue Berry Oat Meal')
+streamlit.text(' 🥗 Kale Spinach and Rocket Smoothy')
+streamlit.text(' 🐔 Hard Boiled Free Range Egg')
+streamlit.text(' 🥑🍞 Avacado Toast')
+
+   
