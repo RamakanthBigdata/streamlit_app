bcc7bcd127749ee7f14b84de75b06f9f87898902
RamakanthBigdata <ramakanth.padala@gmail.com>
2023-07-18 16:36:12 -0500

    Update streamlit_app.py
    Added Header and Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blue Berry Oat Meal')
+streamlit.text('Kale Spinach and Rocket Smoothy')
+streamlit.text('Hard Boiled Free Range Egg')
